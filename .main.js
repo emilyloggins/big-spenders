55f7257f4d9c3fee96af98c6bccd8f9cfb7694d9
Emily Loggins <em.loggins7@gmail.com>
2019-05-01 10:43:37 -0500

    first commit

---
+++ .main.js
@@ -0,0 +1,18 @@
+bigSpenders = [];
+
+const filterFunction = function(order) {
+    if (order >= 9000) {
+        bigSpenders.push(order)
+    } else {
+        order === false;
+    }
+}
+
+const arrayFinder = businesses.filter(business => {
+    const allOrders = business.orders;
+    allOrders.forEach(order => filterFunction(order))
+});
+
+console.log(bigSpenders)
+
+// PRACTICE: SOLAR SYSTEM /////////////////////////
---
+++ README.md
@@ -0,0 +1,4 @@
+# big-spenders
+
+A big spender is anyone who placed an order over $9000.
+Use the filter method to collect all of the big spenders into a new array. 
